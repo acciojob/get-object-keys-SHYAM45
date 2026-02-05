12423c79cd9ac1fefb87983ba8896d9d8115c70f
Shyam Hirulkar <134430246+SHYAM45@users.noreply.github.com>
2026-02-05 18:22:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+const student = {
+	name: "John",
+}
+
+function getKeys(Student){
+	for(let key in Student){
+		return Object.keys(Student)
+	}
+}
+
+console.log(getKeys(Student))
